303e2b9fc43eac6b3c7c1d7384e87117f321d719
Cristian Perilli <cristian.perilli@studenti.ittsrimini.edu.it>
2022-03-15 12:37:08 +0100

    FINE PRORAMMA REGISTRATO

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -43,6 +43,7 @@ namespace CristianPerilli_IIIG
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -169,11 +170,20 @@ namespace CristianPerilli_IIIG
             this.label4.Size = new System.Drawing.Size(0, 15);
             this.label4.TabIndex = 17;
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(590, 288);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 15);
+            this.label8.TabIndex = 18;
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -211,6 +221,7 @@ namespace CristianPerilli_IIIG
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- frmMain.cs
+++ frmMain.cs
@@ -12,18 +12,19 @@ namespace CristianPerilli_IIIG
 {
     public partial class frmMain : Form
     {
-        
+        //CRISTIAN PERILLI IIIG 
         public frmMain()
         {
             InitializeComponent();
-           
+
         }
-        
+        int ricordo1 = 0;
+        int ricordo2 = 0;
+        int ricordo3 = 0;
+
         private void button1_Click(object sender, EventArgs e)
         {
-            int ricordo1 = 0;
-            int ricordo2 = 0;
-            int ricordo3 = 0;
+         
             string a1;
             int a2;
             do { 
@@ -34,18 +35,18 @@ namespace CristianPerilli_IIIG
             
             if (a1 == "Pizza margherita")
             {
-                 ricordo1 = a2 + ricordo1
-                label4.Text = Convert.ToString(a2);
+                ricordo1 = a2 + ricordo1;
+                label4.Text = Convert.ToString(ricordo1);
 
             } else if (a1 == "Pizza 4 stagioni")
             {
                 ricordo2 = a2 + ricordo2;
              
-                label6.Text = Convert.ToString(a2);
+                label8.Text = Convert.ToString(ricordo2);
             }
             else {
                 ricordo3 = a2 + ricordo3; 
-                label7.Text = Convert.ToString(a2); 
+                label7.Text = Convert.ToString(ricordo3); 
             }
 
 
